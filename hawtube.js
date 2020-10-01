7ffbf8f5fd9cd6bb0e19a62aff6afb1264188139
Ian Roberts <irob466@gmail.com>
2020-09-30 19:24:03 -0500

    can't use relative links

--- hawtube.js
+++ hawtube.js
@@ -1,6 +1,8 @@
+const BASE_URL = 'https://irob466.github.io/hawtube'
+
 const silhouette = document.createElement("img");
 
-silhouette.src = "/mst3k.png"
+silhouette.src = `${BASE_URL}/mst3k.png`
 silhouette.style.position = "absolute";
 silhouette.style.bottom = 0;
 silhouette.style.right = "15px";
@@ -8,5 +10,5 @@ silhouette.style.zIndex = 1;
 
 document.querySelector("#videowrap").appendChild(silhouette);
 
-const audio = new Audio('/jackass.mp3');
+const audio = new Audio(`${BASE_URL}/jackass.mp3`);
 audio.addEventListener('canplaythrough', e => audio.play());
