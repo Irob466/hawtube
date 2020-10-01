4afffd62138c93dc9b95f898bf4c0cc8dab2ac3c
Ian Roberts <irob466@gmail.com>
2020-09-30 20:11:05 -0500

    we'll be right back

--- hawtube.js
+++ hawtube.js
@@ -1,14 +1,16 @@
 const BASE_URL = 'https://irob466.github.io/hawtube'
 
+// mst3k silhouette
 const silhouette = document.createElement("img");
-
 silhouette.src = `${BASE_URL}/mst3k.png`
 silhouette.style.position = "absolute";
 silhouette.style.bottom = 0;
 silhouette.style.right = "15px";
 silhouette.style.zIndex = 1;
-
 document.querySelector("#videowrap").appendChild(silhouette);
 
+// jackass intro
 const audio = new Audio(`${BASE_URL}/jackass.mp3`);
 audio.addEventListener('canplaythrough', e => audio.play());
+
+// we'll be right back
