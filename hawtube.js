85c445a637580ee0db8ad21fe2c0d73da1b88e2b
Ian Roberts <irob466@gmail.com>
2020-10-02 16:55:20 -0500

    jackass restored

--- hawtube.js
+++ hawtube.js
@@ -10,30 +10,30 @@ silhouette.style.zIndex = 1;
 document.querySelector("#videowrap").appendChild(silhouette);
 
 // jackass intro
-// const audio = new Audio(`${BASE_URL}/jackass.mp3`);
-// audio.addEventListener('canplaythrough', e => audio.play());
+const audio = new Audio(`${BASE_URL}/jackass.mp3`);
+audio.addEventListener('canplaythrough', e => audio.play());
 
 // we'll be right back
-const brb = new Image();
-brb.src = `${BASE_URL}/wellbrb.png`;
-brb.classList.add('brb');
-const brbSound = new Audio(`${BASE_URL}/wellbrb.mp3`);
+// const brb = new Image();
+// brb.src = `${BASE_URL}/wellbrb.png`;
+// brb.classList.add('brb');
+// const brbSound = new Audio(`${BASE_URL}/wellbrb.mp3`);
 
-const frostedPane = document.createElement('div');
-frostedPane.classList.add('frosted-pane');
-frostedPane.appendChild(brb);
-brbSound.addEventListener('canplaythrough', e => {
-    document.body.appendChild(frostedPane);
-    document.body.style.overflow = 'hidden';
-    window.scrollTo(0, 0);
-    brbSound.play();
-});
+// const frostedPane = document.createElement('div');
+// frostedPane.classList.add('frosted-pane');
+// frostedPane.appendChild(brb);
+// brbSound.addEventListener('canplaythrough', e => {
+//     document.body.appendChild(frostedPane);
+//     document.body.style.overflow = 'hidden';
+//     window.scrollTo(0, 0);
+//     brbSound.play();
+// });
 
-brbSound.addEventListener('ended', e => {
-    setTimeout(() => {
-        document.body.removeChild(frostedPane);
-        document.body.style.overflow = 'visible';
-        frostedPane = null;
-        brb = null;
-    }, 1000);
-});
+// brbSound.addEventListener('ended', e => {
+//     setTimeout(() => {
+//         document.body.removeChild(frostedPane);
+//         document.body.style.overflow = 'visible';
+//         frostedPane = null;
+//         brb = null;
+//     }, 1000);
+// });
